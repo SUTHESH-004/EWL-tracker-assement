2e3a8446339918a150b8967700a663051c881bf2
unknown <sutheshpravin4@gmail.com>
2022-09-22 15:18:42 +0530

    fifth commit

--- JAVASCRIPT.js
+++ JAVASCRIPT.js
@@ -5,20 +5,21 @@ fetch("https://randomuser.me/api/?results=1")
         return response.jason();
     }
     else{
-        throw new Error("Website is error")
+        throw new Error("Website is error");
     }
-}
+})
 
 .then(date=>{
     console.log(data.results);
     display(data.results);
 }
 )
-.catch((error=>console.error("Fetch Error",error));
+.catch((error=>console.error("Fetch Error",error)
 function display(data){
     const name = data[0].namefirst;
-    const namediv =document.getelementById("name1")
-    namediv.innerHTML = name;
+    const namediv =document.getelementById("name1");
+    namediv.innerHTML = name;}
+)
 
-}
+)
 
